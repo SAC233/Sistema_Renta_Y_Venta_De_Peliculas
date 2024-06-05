66929657dce955656e096661cee1e8db26be61fe
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2024-06-05 18:36:46 -0500

    Payment Interface and Service created

---
+++ Sistema_Venta_y_Renta_Peliculas/Domain/Interfaces/IPaymentService.cs
@@ -0,0 +1,13 @@
+﻿using Sistema_Venta_y_Renta_Peliculas.DAL.Entities;
+
+namespace Sistema_Venta_y_Renta_Peliculas.Domain.Interfaces
+{
+    public interface IPaymentService
+    {
+        Task<IEnumerable<Payment>> GetPaymentAsync();
+        Task<Payment> GetPaymentByIdAsync(Guid id);
+        Task<Payment> CreatePaymentAsync(Payment payment);
+        Task<Payment> EditPaymentAsync(Payment payment);
+        Task<Payment> DeletePaymentAsync(Guid id);
+    }
+}
