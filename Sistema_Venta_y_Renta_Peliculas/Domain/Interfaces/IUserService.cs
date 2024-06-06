e73932b55e88712c64d2686ccda5085954435ef2
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2024-06-05 20:41:24 -0500

    Bill Interface and service created

--- Sistema_Venta_y_Renta_Peliculas/Domain/Interfaces/IUserService.cs
+++ Sistema_Venta_y_Renta_Peliculas/Domain/Interfaces/IUserService.cs
@@ -5,12 +5,12 @@ namespace Sistema_Venta_y_Renta_Peliculas.Domain.Interfaces
 {
     public interface IUserService
     {
-        Task<User> GetUserByIdAsync(Guid Id);//
-        Task CheckRoleAsync(string roleName);//
-        Task AddUserToRoleAsync(User user, string roleName);//
-        Task<bool> IsUserInRoleAsync(User user, string roleName);//
-        Task<User> AddUserAsync(User user);//
-        Task<User> UpdateUserAsync(User user);//
-        Task<User> DeleteUserAsync(Guid Id);//
+        Task<User> GetUserByIdAsync(Guid Id);
+        Task CheckRoleAsync(string roleName);//Verifica que el rol existe
+        Task AddUserToRoleAsync(User user, string roleName);//Para agregarlo al rol
+        Task<bool> IsUserInRoleAsync(User user, string roleName);//Para ver que este en el rol asignado
+        Task<User> AddUserAsync(User user);
+        Task<User> UpdateUserAsync(User user);
+        Task<User> DeleteUserAsync(Guid Id);
     }
 }
